822394d6e6592360663fdf85d1b580d9d2578cbe
harshana15 <harshana555prajapati@gmail.com>
2025-04-06 07:56:51 +0530

    feat: read content of file

--- rust_grep/src/main.rs
+++ rust_grep/src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
     
     let query = &args[1];
-    let file_name = &args[2];
+    let file_path = &args[2];
     println!("query: {query}");
-    println!("file_name: {file_name}");
+    println!("In file_name: {file_path}");
+
+    let contents = fs::read_to_string(file_path)
+    .expect("should have been able to read the file");
+    println!("Content inside the file: \n{contents}");
 }
